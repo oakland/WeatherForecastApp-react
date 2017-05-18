e7ca4851afe3473fa5008d14f475d0caa4d1f736
jizq <jizhengqistrongman@126.com>
2017-05-18 11:34:07 +0800

    modify README.md and comment google_map.js

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class GoogleMap extends Component {
+	// ref 属性相关的内容已经发生变化了，之前是用 string 来获取到 ref 的 dom，现在已经采用回调的方式来获取
+	// dom 了。相关内容查看下面两个链接
+	// https://facebook.github.io/react/docs/refs-and-the-dom.html
+	// http://huziketang.com/books/react/lesson21
 	componentDidMount() {
 		new google.maps.Map(this.refs.map, {
 			zoom: 12,
